b17d927a4dc46403cafdeddafb73786cf0ccc3d1
romain <romain.thomine007@gmail.com>
2019-04-19 10:02:39 +0200

    fini

---
+++ mains.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("#btn1").click(function(){
+    $("li").css("color", "black");
+  });
+  $("#btn2").click(function(){
+    $("li").css("color", "red");
+  });
+});
